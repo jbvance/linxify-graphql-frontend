136a24cdd6869763fac070e4069d280a02899a82
Jason Vance <jbvance@gmail.com>
2020-01-03 14:19:32 -0600

    modify AllLinks to display ErrorMessage

--- components/AllLinks.js
+++ components/AllLinks.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import Links from './Links';
+import ErrorMessage from './ErrorMessage';
 import { USER_LINKS_QUERY } from './Links';
 
-const AllLinks = () => {    
-    const { data, loading, error } = useQuery(USER_LINKS_QUERY);
-    if (loading) return <div>Loading...</div>;    
-    return (
+const AllLinks = () => {
+  const { data, loading, error } = useQuery(USER_LINKS_QUERY);
+  if (loading) return <div>Loading...</div>;
+  return (
+    <React.Fragment>
+      <ErrorMessage error={error} />
       <Links links={data.userLinks} />
-    );
+    </React.Fragment>
+  );
 };
 
 export default AllLinks;
