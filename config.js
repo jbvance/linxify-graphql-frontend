5ef08438f5a4c3bfb2e23327ea857d12a5e4790f
jbvance <jbvance@gmail.com>
2020-01-10 08:22:40 -0500

    update perPage in config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 export const prodEndpoint = `https://linxify-dev-backend.herokuapp.com`;
 export const endpoint = `https://linxify-dev-backend.herokuapp.com`;
 //export const endpoint = 'http://port-4444.linxify-graphql-jbvance924063.codeanyapp.com/';
-export const perPage = 4;
+export const perPage = 20;
